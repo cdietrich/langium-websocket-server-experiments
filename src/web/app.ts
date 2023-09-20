6166fcc20bd5e0af6893a7bfba26ff168589236f
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-09-20 18:34:34 +0200

    initial checkin

---
+++ src/web/app.ts
@@ -0,0 +1,87 @@
+
+// import { AbstractMessageReader, AbstractMessageWriter, DataCallback, Message, Disposable } from 'vscode-languageclient/node.js';
+import {  AbstractMessageReader, AbstractMessageWriter, DataCallback, Message, Disposable, createConnection } from 'vscode-languageserver/node.js';
+import { createHelloWorldServices } from '../language/hello-world-module.js';
+import { DefaultSharedModuleContext, EmptyFileSystemProvider, startLanguageServer } from 'langium';
+// import express from 'express';
+// import http from 'http'
+import { WebSocketServer, WebSocket } from 'ws'
+
+//const app = express();
+
+// initialize a simple http server
+//const server = http.createServer(app);
+
+// initialize the WebSocket server instance
+
+
+
+
+class WebSocketMessageWriter extends AbstractMessageWriter {
+    private readonly ws: WebSocket;
+
+    constructor(ws: WebSocket) {
+        super();
+        this.ws = ws;
+    }
+
+    write(msg: Message): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const content = JSON.stringify(msg);
+            this.ws.send(content, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    end(): void {}
+}
+
+class WebSocketMessageReader extends AbstractMessageReader {
+    private readonly ws: WebSocket;
+
+    constructor(ws: WebSocket) {
+        super();
+        this.ws = ws;
+    }
+
+    listen(callback: DataCallback): Disposable {
+        const messageListener = (rawMessage: Message) => {
+            const message = JSON.parse(rawMessage.toString()) as Message;
+            callback(message);
+        };
+        const closeListener = () => this.fireClose()
+
+        this.ws.on('message', messageListener);
+        this.ws.on('close', closeListener);
+
+        const d: Disposable = {
+            dispose: () => {
+                this.ws.removeListener('message', messageListener);
+                this.ws.removeListener('close', closeListener);
+            }
+        }
+        return d;
+    }
+}
+
+// const app = express();
+// const server = http.createServer(app);
+
+const wsServer = new WebSocketServer({ port: 3033 });;
+//const websocketServer = new ws.WebSocket.Server({ port: 3033 });
+wsServer.on('connection', (ws, request) => {
+    const reader = new WebSocketMessageReader(ws);
+    const writer = new WebSocketMessageWriter(ws);
+    const connection = createConnection(reader, writer);
+
+    const ctx: DefaultSharedModuleContext = { connection, fileSystemProvider: () => new EmptyFileSystemProvider() };
+    const services = createHelloWorldServices(ctx);
+    startLanguageServer(services.shared);
+});
+// server.listen(process.env.PORT || 8080, () =>{
+//      console.log('Server started');});
